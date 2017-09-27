f75623db89c8b57d3f0f1cb8f00a5da8e5fdc8b5
kagose yusuke <>
2017-09-27 12:08:28 +0900

    implemented custom scope for portfolio features

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,6 +3,10 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+  def angular
+    @angular_portfolio_items = Portfolio.angular
+  end
+
   def show
     @portfolio_item = Portfolio.find(params[:id])
   end
