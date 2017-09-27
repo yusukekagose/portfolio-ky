54a18c1abd735a5368cee55ae55c4e52c584053a
kagose yusuke <>
2017-09-27 20:59:52 +0900

    implemented strongparameter for portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,17 +7,13 @@ class PortfoliosController < ApplicationController
     @angular_portfolio_items = Portfolio.angular
   end
 
-  def show
-    @portfolio_item = Portfolio.find(params[:id])
-  end
-
   def new
     @portfolio_item = Portfolio.new
     3.times { @portfolio_item.technologies.build }
   end
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+    @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
@@ -36,12 +32,16 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
 
     respond_to do |format|
-      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+      if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
       else
         format.html { render :edit }
       end
     end
+  end
+
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def destroy
@@ -57,6 +57,14 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  private
 
+  def portfolio_params
+    params.require(:portfolio).permit(:title,
+                                      :subtitle,
+                                      :body,
+                                      technologies_attributes: [:name]
+                                     )
+  end
 
 end
