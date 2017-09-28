b3f44f0a9d386438dd579120025263de0d4f47b3
kagose yusuke <>
2017-09-28 17:15:06 +0900

    install gem copyright generator

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,7 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+  def copyright_generator
+    DevcampViewTool::Renderer.copyright 'Yusuke', 'All rights reserved'
+  end
 end
