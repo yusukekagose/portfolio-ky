d074c9e688f0148441b9bd7eacf2b1262b2642c6
kagose yusuke <>
2017-09-28 18:12:08 +0900

    implemented atuhorization engine for portfolio and blog

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,7 @@
 class PortfoliosController < ApplicationController
   layout "portfolio"
   before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
+  access all: [:show, :index, :angular], user: {except: [:destroy,:new,:create, :update, :edit]}, site_admin: :all
 
   def index
     @portfolio_items = Portfolio.all
