3bac592b8f4c8e36b2c850a739bb10716b5920e7
kagose yusuke <>
2017-09-29 21:12:14 +0900

    carriewave implementation

--- app/uploaders/portfolio_uploader.rb
+++ app/uploaders/portfolio_uploader.rb
@@ -5,7 +5,7 @@ class PortfolioUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :aws
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
