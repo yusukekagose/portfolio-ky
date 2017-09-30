610fa592b401197aba8111c8d450b0e35caeabc9
kagose yusuke <>
2017-09-30 22:39:32 +0900

    blog weget for topic

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,12 +1,13 @@
 class TopicsController < ApplicationController
 
-  layout "blog"
+  before_action :set_sidebar_topics
+  layout 'blog'
   def index
     @topics = Topic.all
   end
 
   def show
-     @topic = Topic.find(params[:id])
+    @topic = Topic.find(params[:id])
 
     if logged_in?(:site_admin)
       @blogs = @topic.blogs.recent.page(params[:page]).per(5)
@@ -15,5 +16,9 @@ class TopicsController < ApplicationController
     end
   end
 
+  private
 
+  def set_sidebar_topics
+    @side_bar_topics = Topic.with_blogs
+  end
 end
