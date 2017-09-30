e0438439038f68150788a789a43d648e0062b506
kagose yusuke <>
2017-09-30 13:16:20 +0900

    refactored alert to helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -57,4 +57,19 @@ module ApplicationHelper
     "active" if current_page? path
   end
 
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert
+    end
+  end
+
+  def alert_generator msg
+     js add_gritter(msg, title: "", sticky: false, :time => 2000)
+  end
+
+
+
+
 end
